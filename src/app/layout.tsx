0f12d7272ccac828bb14a091a53d9b80729a5ba9
Bakk Attila <attila.bakk11@gmail.com>
2025-03-10 22:21:31 +0100

    verify_2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="Uk1V7NFyu8W5TuJKt8ZjZl1N82kpA6QCmoDtxjiBj-8" />
         <title>Fóti Roland - Ipari Alpinista</title>
         <link rel="icon" href="/favicon.ico" />
         <link
